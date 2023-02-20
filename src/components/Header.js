f2f30eba870d228982923b7c17227474b1345634
Donmark2k <chukwuemekavp@gmail.com>
2023-02-20 15:19:18 +0100

    Create header file

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+      <header
+        style={{
+          padding: '20px 0',
+          lineHeight: '1.5em',
+          color: '#aeadad',
+          textAlign: 'center',
+        }}
+      >
+        {/* ... */}
+      </header>
+    );
+  };
+  export default Header;
+  
