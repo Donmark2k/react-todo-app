5d887814f6f16c4cc0bacf2f3f4270bb8aa472cf
Donmark2k <chukwuemekavp@gmail.com>
2023-02-20 11:18:42 +0100

    adding a checked and onChange attributes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,11 @@
 const TodoItem = ({ itemProp, setTodos }) => {
     return (
       <li>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => console.log('clicked')}
+        />
         {itemProp.title}
       </li>
     );
